bf1b06dbc6feea768230876d5d6357dc2591fed5
saleemhamo <saleemhamo@gmail.com>
2023-10-31 12:19:54 +0000

    main method code

--- helper.py
+++ helper.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+
 def read_ecg_data():
     ecg_data_path = "ecg.dat"
 
@@ -15,3 +20,33 @@ def read_ecg_data():
                 print(f"Ignoring non-numeric line: {line.strip()}")
 
     return numeric_records
+
+
+def convert_to_time(number_of_samples, sample_rate):
+    stop = number_of_samples / sample_rate
+    return np.linspace(0, stop, number_of_samples)
+
+
+def mkdir_if_not_exists(directory):
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+        print(f"Directory '{directory}' created.")
+    else:
+        print(f"Directory '{directory}' already exists.")
+
+
+def plot(x, y, title, x_label, y_label, plot_name='', log_scale=False):
+    plt.figure(figsize=(10, 6))
+    plt.plot(y, x)
+    plt.title(title)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    # plt.grid(True)
+    if log_scale:
+        plt.xscale('log')
+
+    plt.show()
+
+    if not (plot_name is None or plot_name == ''):
+        mkdir_if_not_exists('./output_images')
+        plt.savefig('./output_images/' + plot_name + ".svg", format='svg')
--- hpbsfilter.py
+++ hpbsfilter.py
@@ -1,7 +1,19 @@
 # hpbsfilter.py is the main program which uses firfilter to remove DC and 50Hz (1-3
 
-def main():
-    pass
+import helper
 
+
+def main():
+    ecg_data = helper.read_ecg_data()
+    time = helper.convert_to_time(len(ecg_data), 1000)
+
+    # Plot the original data
+    helper.plot(ecg_data, time, 'ECG Data (Original)', 'Time (s)', 'Amplitude')
+
+    # data
+    # highpass_cofficients = highpass_design(....)
+    # band_stop_cofficients = band_stop_design(....)
+    # fir_filter(highpass, data)
+    # fir_filter(band_stop_cofficients, data)
 
 main()
